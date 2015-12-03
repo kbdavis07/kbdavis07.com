80c76b025115a591986ec4fb6f35fb66d80617b7
kbdavis07 <kbdavis07@yahoo.com>
2015-12-03 15:12:27 -0500

    1. Start

---
+++ KB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KB.Startup))]
+namespace KB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
