f39e31aba4e9aec8103421923331adf893de8bcc
kbdavis07 <kbdavis07@yahoo.com>
2016-01-11 10:01:14 -0500

    Updates to Tools pages

---
+++ KB/Tools/js/CustomElements.js
@@ -0,0 +1,20 @@
+﻿
+
+//*Creates Custom Elements
+
+///<container>
+document.createElement("container");
+
+///<sidebar>
+document.createElement("sidebar");
+
+///<widget-zone
+document.createElement("widget-zone");
+
+///<widget>
+document.createElement("widget");
+
+///<ad>
+document.createElement("ad");
+
+
