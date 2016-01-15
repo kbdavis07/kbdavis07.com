ae6fffa69004e8f168327b6e595864533ffbd937
kbdavis07 <kbdavis07@yahoo.com>
2016-01-15 16:45:19 -0500

    Added Tracking scripts.
    Deploying to server

---
+++ KB/Scripts/app/statscounter.js
@@ -0,0 +1,19 @@
+﻿//Start of StatCounter Code for Default Guide
+<script type="text/javascript">
+var sc_project=10783087; 
+var sc_invisible=1; 
+var sc_security="1fb47a88"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="site stats"
+href="http://statcounter.com/free-web-stats/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/10783087/0/1fb47a88/1/"
+alt="site stats"></a></div></noscript>
+
+
+ //End of StatCounter Code for Default Guide
